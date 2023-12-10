f8b7c5962dc5471b7528c51275105577019ce101
unknown <dovandaihoc@gmail.com>
2023-12-10 17:16:30 +0700

    .

--- NguyenVietTu_211200970/Controllers/HomeController.cs
+++ NguyenVietTu_211200970/Controllers/HomeController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Drawing.Printing;
+using System.Security.Cryptography;
 
 namespace NguyenVietTu_211200970.Controllers
 
@@ -11,19 +13,56 @@ namespace NguyenVietTu_211200970.Controllers
 	{
 
         private QLHangHoaContext db;
-        private static int limit = 6;
+        private static int limit = 2;
         public HomeController(QLHangHoaContext context)
         {
             db = context;
         }
 
-        public IActionResult Index()
+		public IActionResult Index(int? lh)
 		{
-			IEnumerable<HangHoa> list_hh = (IQueryable<HangHoa>)db.HangHoas.Where(hh => hh.Gia > 100).ToList();
-			return View("NguyenVietTu_mainContent", list_hh);
+			IEnumerable<HangHoa> list_hh;
+
+			if (lh.HasValue && lh > 0)
+			{
+				list_hh = db.HangHoas.Where(hh => hh.Gia > 100 && hh.MaLoai.Equals(lh.Value)).ToList();
+			}
+			else
+			{
+				list_hh = db.HangHoas.Where(hh => hh.Gia > 100).ToList();
+			}
+
+			int pageNumber = (int)Math.Ceiling((double)list_hh.Count() / limit);
+			ViewBag.pageNum = pageNumber;
+			return View("NguyenVietTu_mainContent", list_hh.Take(limit));
 		}
 
-        public IActionResult ShowProducts(int lh) 
+		public IActionResult ShowProductsFilter(int? lh, string? keyword, int page = 1)
+		{
+			IEnumerable<HangHoa> list_hh = (IQueryable<HangHoa>)db.HangHoas.Where(hh => hh.Gia > 100);
+			
+			if(lh > 0)
+            {
+				list_hh = list_hh.Where(hh => hh.MaLoai.Equals(lh));
+				ViewBag.lh = lh;
+			}
+
+			if(keyword != null)
+			{
+				list_hh = list_hh.Where(x => x.TenHang.ToLower().Contains(keyword.ToLower()));
+                ViewBag.keyword = keyword;
+			}
+
+			int pageNum = (int)Math.Ceiling(list_hh.Count() / (float)limit);
+			ViewBag.pageNum = pageNum;
+
+			var result = list_hh.Skip(limit * (page - 1)).Take(limit);
+
+			return PartialView("ListProduct", result); // html
+		}
+
+
+		public IActionResult ShowProducts(int lh) 
         {
 			IEnumerable<HangHoa> list_hh = db.HangHoas.Where(hh => hh.Gia > 100).ToList();
 
